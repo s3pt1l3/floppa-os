51cf2b89d76737ad80a20ed0896e2289644c10fa
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2022-03-24 15:09:57 +0300

    Feat(kernel, utilities): add to_string functions (convert number to string)

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -20,5 +20,19 @@ extern "C" void _start(Framebuffer* framebuffer, PSF1_FONT* psf1_font) {
     }*/
     BasicRenderer renderer = BasicRenderer(framebuffer, psf1_font);
 
+    renderer.Print(to_string((uint64_t)1234976));
+    renderer.CursorPosition = { 0, 16 };
+    renderer.Print(to_string((int64_t)-1234976));
+    renderer.CursorPosition = { 0, 32 };
+    renderer.Print(to_string((double)-13.14));
+    renderer.CursorPosition = { 0, 48 };
+    renderer.Print(to_hstring((uint64_t)0xF0));
+    renderer.CursorPosition = { 0, renderer.CursorPosition.Y + 16 };
+    renderer.Print(to_hstring((uint32_t)0xF0));
+    renderer.CursorPosition = { 0, renderer.CursorPosition.Y + 16 };
+    renderer.Print(to_hstring((uint16_t)0xF0));
+    renderer.CursorPosition = { 0, renderer.CursorPosition.Y + 16 };
+    renderer.Print(to_hstring((uint8_t)0xF0));
+
     return;
 }
