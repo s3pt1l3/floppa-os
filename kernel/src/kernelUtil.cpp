117038afaf7d338522a3ff156d49a71affa5e36a
TriangulumGalaxy <59769865+TriangulumGalaxy@users.noreply.github.com>
2022-03-30 00:19:40 +0300

    feat: add PCI devices descriptors

--- kernel/src/kernelUtil.cpp
+++ kernel/src/kernelUtil.cpp
@@ -70,6 +70,7 @@ void PrepareACPI(BootInfo* bootInfo) {
     // for (int t = 0; t < 4; t++){
     //     GlobalRenderer->PutChar(mcfg->Header.Signature[t]);
     // }
+        
 
     PCI::EnumeratePCI(mcfg);
 }
