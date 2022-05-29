f26e6acc82b20362ce90811ff7b412f2b3f7e978
TriangulumGalaxy <59769865+TriangulumGalaxy@users.noreply.github.com>
2022-05-29 17:41:51 +0300

    feat: change cursor

--- kernel/src/userinput/mouse.cpp
+++ kernel/src/userinput/mouse.cpp
@@ -1,22 +1,22 @@
 #include "mouse.h"
 
 uint8_t MousePointer[] = {
-    0b11111111, 0b00000000,
-    0b11111111, 0b00000000,
-    0b11000000, 0b00000000,
-    0b11000000, 0b00000000,
-    0b11111111, 0b00000000,
-    0b11111111, 0b00000000,
-    0b11000000, 0b00000000,
-    0b11000000, 0b00000000,
-    0b11000000, 0b00000000,
-    0b11000000, 0b00000000,
-    0b11000000, 0b00000000,
-    0b11000000, 0b00000000,
-    0b00000000, 0b00000000,
-    0b00000000, 0b00000000,
-    0b00000000, 0b00000000,
-    0b00000000, 0b00000000,
+    0b00110000, 0b00000000,
+    0b00101000, 0b00000000,
+    0b00100100, 0b00000000,
+    0b00100010, 0b00000000,
+    0b00100001, 0b00000000,
+    0b00100000, 0b10000000,
+    0b00100000, 0b01000000,
+    0b00100000, 0b00100000,
+    0b00100000, 0b00010000,
+    0b00100000, 0b00001000,
+    0b00100000, 0b00000100,
+    0b00100000, 0b01111100,
+    0b00100010, 0b01000000,
+    0b00100101, 0b00100000,
+    0b00101001, 0b00100000,
+    0b00110000, 0b11100000,
 };
 
 
@@ -144,7 +144,7 @@ void ProcessMousePacket() {
     if (MousePosition.Y > GlobalRenderer->TargetFramebuffer->Height - 1) MousePosition.Y = GlobalRenderer->TargetFramebuffer->Height - 1;
 
     GlobalRenderer->ClearMouseCursor(MousePointer, MousePositionOld);
-    GlobalRenderer->DrawOverlayMouseCursor(MousePointer, MousePosition, 0xdb5ac1);
+    GlobalRenderer->DrawOverlayMouseCursor(MousePointer, MousePosition, 0xffffff);
 
     if (MousePacket[0] & PS2Leftbutton) {
         uint32_t color = GlobalRenderer->Color;
